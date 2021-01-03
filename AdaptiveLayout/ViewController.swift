ef960046ab682784d8da3311788791b6c58b54a0
arpit-garg-1995 <appyimproviser@gmail.com>
2021-01-03 19:45:56 +0530

    initial commit, adaptive layout with adaptive font size

--- AdaptiveLayout/ViewController.swift
+++ AdaptiveLayout/ViewController.swift
@@ -9,11 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var IBConstraints: [NSLayoutConstraint]!
+    @IBOutlet weak var imageSuperView: UIView!
+    var landscapeConstraints = [NSLayoutConstraint]()
+    @IBOutlet weak var otherView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        landscapeConstraints.append(view.bottomAnchor.constraint(equalTo: imageSuperView.bottomAnchor))
+        landscapeConstraints.append(imageSuperView.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.3))
+        landscapeConstraints.append(imageSuperView.trailingAnchor.constraint(equalTo: otherView.leadingAnchor))
+        landscapeConstraints.append(otherView.topAnchor.constraint(equalTo: view.topAnchor))
     }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        if UIDevice.current.orientation.isLandscape{
+            applyLandscapeConstraints()
+        }else{
+            portraitConstraints()
+        }
+    }
+    
+    private func portraitConstraints(){
+        if let consts = IBConstraints{
+            NSLayoutConstraint.deactivate(landscapeConstraints)
+            view.addConstraints(consts)
+        }
+        
+    }
+    
+    private func applyLandscapeConstraints(){
+        if let consts = IBConstraints{
+            NSLayoutConstraint.deactivate(consts)
+            view.addConstraints(landscapeConstraints)
+        }
+    }
+    
+    
 
 
 }

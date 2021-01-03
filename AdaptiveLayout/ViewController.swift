d205f39ff4205c5a758d868de49ce86dc46e5a8a
arpit-garg-1995 <appyimproviser@gmail.com>
2021-01-03 11:22:46 +0530

    Initial Commit

---
+++ AdaptiveLayout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdaptiveLayout
+//
+//  Created by Arpit Garg on 03/01/21.
+//  Copyright © 2021 Arpit Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
